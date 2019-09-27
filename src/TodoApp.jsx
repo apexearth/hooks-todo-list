a4a722042c7aa21d09fc8a4fa6b5775d477860f5
Christopher Jacobs <apexearth@gmail.com>
2019-09-26 17:30:40 -0700

    Changes for a tiny todo app.

---
+++ src/TodoApp.jsx
@@ -0,0 +1,62 @@
+import React, {Component, useState} from 'react';
+import './TodoApp.scss';
+
+export default class TodoApp extends Component {
+  state = {
+    todos: [
+      {name: 'Wash Dishes', complete: false},
+      {name: 'Make the Bed', complete: true},
+      {name: 'Count things!', complete: false},
+    ],
+  };
+
+  onAdd = (name) => {
+    if (!name) {
+      return
+    }
+    this.setState((state) => ({
+      todos: [...state.todos, {name, complete: false}]
+    }));
+  };
+
+  toggleComplete = (todo) => {
+    todo.complete = !todo.complete;
+    this.setState((state) => ({
+      todos: [...state.todos]
+    }));
+  };
+
+  render() {
+    return <div className={'todo-app'}>
+      <div>
+        <TodoForm onAdd={this.onAdd}/>
+        <TodoList todos={this.state.todos} toggleComplete={this.toggleComplete}/>
+      </div>
+    </div>;
+  }
+
+}
+
+const TodoForm = ({onAdd}) => {
+  const [text, setText] = useState('');
+  return <div className={'todo-form'}>
+    <span>Add item:</span>
+    <div>
+      <input type="text" value={text} onChange={e => setText(e.target.value)}/>
+      <button onClick={() => onAdd(text)}>Add</button>
+    </div>
+  </div>;
+};
+
+const TodoList = ({todos, toggleComplete}) => (
+  <div className={'todo-list'}>
+    {todos.map(todo => <Todo todo={todo} toggleComplete={toggleComplete}/>)}
+  </div>
+);
+
+const Todo = ({todo, toggleComplete: toggleComplete}) => (
+  <div className={'todo'}>
+    <div className={todo.complete && 'todo-complete'}>{todo.name}</div>
+    <button onClick={() => toggleComplete(todo)}>{todo.complete ? 'incomplete' : 'complete'}</button>
+  </div>
+);
